a94019e28bda9625c7a050bf6c0ceca6f634bc98
Lars Gohr <larsgohr@gmail.com>
2017-05-27 22:11:57 +0200

    Using styled Notification

--- src/main/groovy/com/elgohr/concourse/notifier/notifications/NotificationFactoryImpl.groovy
+++ src/main/groovy/com/elgohr/concourse/notifier/notifications/NotificationFactoryImpl.groovy
@@ -9,7 +9,6 @@ class NotificationFactoryImpl implements NotificationFactory {
     def notificationJobs = Executors.newCachedThreadPool()
 
     def createNotification(String name, String pipeline, String url, String status) {
-        def title = "$pipeline - $name"
-        notificationJobs.submit({ -> new Notification(title, "", status) })
+        notificationJobs.submit({ -> new Notification(pipeline, name, status) })
     }
 }
